689d78599839751e2d3af3b51410801d4bfa0db2
wangyan <wyvernnot@gmail.com>
2016-03-18 15:36:53 +0800

    implementation using xinput

---
+++ index.js
@@ -0,0 +1,38 @@
+var keycode = require('keycode');
+var StatsD = require('oneapm-ci-sdk').StatsD;
+var util = require('./lib');
+var statsd = new StatsD('127.0.0.1', 8251);
+var rules = require('./lib/rules');
+
+var statics = util.read();
+console.log('read=', statics);
+
+var tick = function () {
+  util.write(statics);
+  Object.keys(statics).forEach(function (key) {
+    statsd.gauge(`ergonomics.${key}`, statics[key]);
+  });
+}
+
+var processLine = function (line) {
+  rules.forEach(function (rule) {
+    var matchedResult = line.match(rule.regex);
+    if (matchedResult) {
+      if (!statics[rule.key]) {
+        statics[rule.key] = 0;
+      }
+      statics[rule.key]++;
+      // @TODO process rest matched result
+    }
+  });
+}
+
+exports.initWithDeviceIDs = function (devices) {
+  if (!devices) {
+    devices = [9, 10];
+  }
+  devices.forEach(function (id) {
+    util.registerDevice(id, processLine);
+  })
+  setInterval(tick, 5000);
+}
